f85988ad9e05cd35ac78a34dda1add12370e3d6c
aa3447 <jacksonjam1@yahoo.com>
2025-01-26 16:40:38 -0500

    Added letter count fuction

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
-    print(word_count(text))
+    print(f"This book has {word_count(text)} words!")
+    print(f"This book letter count is {letter_count(text)}!")
     #print(text)
 
 def word_count(text):
     words = text.split()
     return len(words)
+
+def letter_count(text):
+    letters = {}
+    for letter in text:
+        if letter.isalpha() or letter.isspace():
+            letter = letter.lower()
+            if letter in letters:
+                letters[letter] += 1
+            else:
+                letters[letter] = 1
+    return letters
 
 def get_book_text(path):
     with open(path) as book:
